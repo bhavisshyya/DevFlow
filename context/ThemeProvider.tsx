327db118134e1897a43acd00025459f33e8f7fa9
bhavisshyya <bhavisshyya@gmail.com>
2024-10-16 18:51:35 +0530

    theme

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,23 +12,28 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("light");
   const handleThemeChange = () => {
-    if (mode === "dark") {
+    if (
+      localStorage.theme === "dark" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
       setMode("light");
       document.documentElement.classList.add("light");
     } else {
       setMode("dark");
       document.documentElement.classList.add("dark");
     }
+  }
+
+    useEffect(() => {
+      handleThemeChange();
+    }, [mode]);
+    return (
+      <ThemeContext.Provider value={{ mode, setMode }}>
+        {children}
+      </ThemeContext.Provider>
+    );
   };
 
-  useEffect(() => {}, [mode]);
-
-  return (
-    <ThemeContext.Provider value={{ mode, setMode }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-}
 
 export function useTheme() {
   const context = useContext(ThemeContext);
